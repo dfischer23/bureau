e67a69cc0206f3112e310916350bda473ca4d280
Daniel Fischer <fischer@interaktiva.de>
2020-01-31 15:17:13 +0100

    use /data/db.sqlite3

--- bureau/settings.py
+++ bureau/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'bureau.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join('/data', 'db.sqlite3'),
     }
 }
 
